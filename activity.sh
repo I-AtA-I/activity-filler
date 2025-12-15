04dfe0a57b3703a60b31000303365de32a1a6259
Vít Sedláček <vitsedl@seznam.cz>
2025-12-15 21:08:45 +0100

    Update activity.sh

--- activity.sh
+++ activity.sh
@@ -25,7 +25,7 @@ chmod +x crontab.sh
 
 echo "Input your username you are currently logged in as: "
 read user
-( crontab -l 2>/dev/null; echo "0 */6 * * * /home/$user/Streakerkeeper/uploader.sh" ) | crontab -
+( crontab -l 0>/dev/null; echo "0 */6 * * * /home/$user/Streakerkeeper/uploader.sh" ) | crontab -
 
 
 #creating uploader.sh file
@@ -40,8 +40,13 @@ echo "$randon" >> ~/streak/README.md
 cd ~/streak
 
 
+echo "$randon" > "/home/$user/streak/README.md"
 
-echo "git add README.md" > "/home/$user/Streakerkeeper/uploader.sh"
+
+echo "randon=$(($RANDOM + $RANDOM * $RANDOM))" > "/home/$user/Streakerkeeper/uploader.sh"
+echo "echo $randon" >> "/home/$user/Streakerkeeper/uploader.sh"
+
+echo "git add README.md" >> "/home/$user/Streakerkeeper/uploader.sh"
 echo "git commit -m $randon" >> "/home/$user/Streakerkeeper/uploader.sh"
 echo 'git push' >> "/home/$user/Streakerkeeper/uploader.sh"
 
